152f312274939f532535e4d8a51eaeb1019486cb
shpp-krusso <shpp-krusso@programming.kr.ua>
2016-07-04 22:27:20 +0300

    Add files via upload

--- a3-huffman/main.cpp
+++ a3-huffman/main.cpp
@@ -3,11 +3,30 @@
 
 int main(int argc, char *argv[])
 {
-    string pathOfOrigin = "/home/kocmuk/Qt-projects/huffman/files/2dyadi.jpg";
-    string pathOfArchive = pathOfOrigin + ".myzip";
-    string pathOfUncompressedFile = pathOfOrigin + "_unzipped";
-    Archiver arch;
-    arch.compress(pathOfOrigin, pathOfArchive);
-    arch.uncompress(pathOfArchive, pathOfUncompressedFile);
+    cout << "If you want to compress the file enter -zip" << endl <<
+            "If you want to uncompress the file enter -unzip" << endl << "Enter your choice:";
+    string command;
+    cin >> command;
+    if (command == "-zip") {
+        cout << "Enter path of the origin..." << endl << "Enter the path:";
+        string pathOfOrigin;
+        cin >> pathOfOrigin;
+        string pathOfArchive = pathOfOrigin + ".myzip";
+        Archiver arch;
+        arch.compress(pathOfOrigin, pathOfArchive);
+        cout << "Done!" << endl;
+    }
+    if (command == "-unzip") {
+        cout << "Enter path of the archive .myzip ..." << endl;
+        string pathOfArchive;
+        cin >> pathOfArchive;
+        string pathOfUncompressedFile = pathOfArchive + "_unzipped";
+        Archiver arch;
+        arch.uncompress(pathOfArchive, pathOfUncompressedFile);
+        cout << "Done!" << endl;
+    } else {
+        cout << "The command is not supported! Please, enter a right command." << endl;
+    }
+
     return 0;
 }
